ae3075372d967c1eca0c4124ad9874c0e94025dd
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2024-09-17 10:06:57 +0200

    fix: staticfiles folder outside

--- django_cms_am/settings.py
+++ django_cms_am/settings.py
@@ -119,9 +119,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/../staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 
-#if DEBUG:
+# if DEBUG:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
