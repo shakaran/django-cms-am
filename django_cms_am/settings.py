8bb261298c69005e9956f7be78093a7c337ee1d4
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2024-09-17 10:04:38 +0200

    fix: allow static files dirs for test ci

--- django_cms_am/settings.py
+++ django_cms_am/settings.py
@@ -121,8 +121,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/../staticfiles')
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#if DEBUG:
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
