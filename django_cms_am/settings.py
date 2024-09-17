155bfd88de03ba65195ba891d8f2468c3b154b9d
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2024-09-17 10:16:01 +0200

    fix: typo possible wrong folder writing

--- django_cms_am/settings.py
+++ django_cms_am/settings.py
@@ -119,7 +119,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # if DEBUG:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
