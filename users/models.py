29791883f92acdcb08d765883d719cb61c7a8f28
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2024-09-24 18:45:37 +0200

    feat: implement soft delete users

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_softdelete.models import SoftDeleteModel
 
-
-class UserProfile(models.Model):
+class UserProfile(SoftDeleteModel, models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_admin = models.BooleanField(default=False)
