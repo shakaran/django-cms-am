d19258b5ee492aa4b9221a7be782ef11471a3f65
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2025-07-09 19:59:13 +0200

    feat: add app label for UserProfile

--- users/models.py
+++ users/models.py
@@ -6,3 +6,8 @@ from django_softdelete.models import SoftDeleteModel
 class UserProfile(SoftDeleteModel, models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'users'
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
