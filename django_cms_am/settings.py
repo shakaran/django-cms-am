4108dcedc5f15af423e64eb2a4bc8e099e48a0cc
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2025-07-09 19:11:55 +0200

    feat: add config for logging

--- django_cms_am/settings.py
+++ django_cms_am/settings.py
@@ -162,3 +162,22 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
